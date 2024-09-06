121afb31457b298c75bcae36e7c7435a3321a53f
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-09-06 23:47:38 +0300

    Reduced the number of queries and used a shortcut.

--- blog/views.py
+++ blog/views.py
@@ -66,8 +66,6 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
-
     related_tags = post.tags.all()
 
     serialized_post = {
@@ -75,7 +73,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -102,7 +100,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     popular_tags = Tag.objects.popular()[:5]
     most_popular_tags = popular_tags
